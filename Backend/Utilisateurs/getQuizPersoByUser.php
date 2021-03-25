cd79c809e6891289d7c650876041f0893fb44d4f
mathieurudy12 <66941346+mathieurudy12@users.noreply.github.com>
2021-03-25 20:06:14 +0100

    Update : getQuizPersoByUser final

--- Backend/Utilisateurs/getQuizPersoByUser.php
+++ Backend/Utilisateurs/getQuizPersoByUser.php
@@ -3,14 +3,18 @@
 include("../Connexion/connexion.php");
 
 $mail = $_GET["mail"];
-  $queryResult = $connection->prepare("SELECT NOMQUIZ, count(*) as NBQUESTIONSQUIZ FROM QUIZ_PERSONNALISE Q JOIN CONTENIR C ON C.IDQUIZPERSO = Q.IDQUIZPERSO WHERE MAILUTILISATEUR = ?");
+  $queryResult = $connection->prepare("SELECT NOMQUIZ, count(*) as NBQUESTIONSQUIZ FROM QUIZ_PERSONNALISE Q JOIN CONTENIR C ON C.IDQUIZPERSO = Q.IDQUIZPERSO WHERE MAILUTILISATEUR = ? GROUP BY NOMQUIZ");
   $queryResult->execute(array($mail));
 
   $result = array();
-
+    $i = 1;
   while ($fetchdata = $queryResult->fetch()) {
-      $result["NOMQUIZ"] = $fetchdata["NOMQUIZ"];
-      $result["NBQUESTIONSQUIZ"] = $fetchdata["NBQUESTIONSQUIZ"];
+      $titre = "Quiz".$i;
+      $arrayt = array();
+      $arrayt["NOMQUIZ"] = $fetchdata["NOMQUIZ"];
+      $arrayt["NBQUESTIONSQUIZ"] = $fetchdata["NBQUESTIONSQUIZ"];
+      $result[$titre] = $arrayt;
+      $i++;
   }
 
   echo json_encode($result);
