0dc9faa91dd1ead35dbb5f477112376e33df3a10
colinAcerW <colin.defever@hotmail.fr>
2021-04-09 12:02:50 +0200

    page accueil

--- clientFlutter/flutter_quizinmobile/lib/main.dart
+++ clientFlutter/flutter_quizinmobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quizinmobile/secondPage.dart';
+import 'package:flutter/src/material/icons.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,20 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Rudy trop beau'),
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        home: MyHomePage(title: 'Rudy trop beau'),
         routes: {
           //'/': (context) => MyHomePage(title: "Connexion"),
           '/secondPage': (context) => MySecondPage(),
@@ -52,43 +42,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            //print('Hello, World!');
-            Navigator.pushNamed(context, '/secondPage');
-          },
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-
-    );
+        body: Center(
+          child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(25),
+                  child: IconButton(
+                    icon: const Icon(Icons.account_circle),
+                    color: Colors.black,
+                    iconSize: 50,
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25),
+                  child: ElevatedButton(
+                    child: Column(children: <Widget>[
+                      Container(child: Text('Mode Standard', style: TextStyle(fontSize: 20.0),),),
+                      Container(child: Text('Rejoindre une partie rapide', style: TextStyle(fontSize: 12.0),),),
+                    ]),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red, // background
+                      onPrimary: Colors.white, // foreground
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25),
+                  child: ElevatedButton(
+                    child: Column(children: <Widget>[
+                      Container(child: Text('Mode Battle Royal', style: TextStyle(fontSize: 20.0),),),
+                      Container(child: Text('Rejoindre une partie rapide', style: TextStyle(fontSize: 12.0),),),
+                    ]),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red, // background
+                      onPrimary: Colors.white, // foreground
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25),
+                  child: ElevatedButton(
+                    child: Column(children: <Widget>[
+                      Container(child: Text('Partie privée', style: TextStyle(fontSize: 20.0),),),
+                      Container(child: Text('Rejoindre le menu de partie privée', style: TextStyle(fontSize: 12.0),),),
+                    ]),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red, // background
+                      onPrimary: Colors.white, // foreground
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ]
+          ),
+        ));
   }
 }
