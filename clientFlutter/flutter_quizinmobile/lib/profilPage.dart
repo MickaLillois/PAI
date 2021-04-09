65e8f8360fb2fb3f5a86b80a67787c2710ba43ec
mathieurudy12 <66941346+mathieurudy12@users.noreply.github.com>
2021-04-09 15:01:13 +0200

    UPDATE : Profil page

--- clientFlutter/flutter_quizinmobile/lib/profilPage.dart
+++ clientFlutter/flutter_quizinmobile/lib/profilPage.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 class ProfilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double sizeAvatar = screenSize.width * 0.3;
+    double widthLogo = screenSize.width * 0.8;
+    double heightLogo = screenSize.height * 0.2;
     return Scaffold(
       body :
       Container(
@@ -12,9 +16,12 @@ class ProfilPage extends StatelessWidget {
             Column(
               children: [
                 Container(
-                  child: Image.asset('assets/images/logo_with_text.PNG'
+                    child: Image.asset(
+                      'assets/images/logo_png.png',
+                      width : widthLogo,
+                      height: heightLogo,
 
-                  )
+                    )
                 )
               ],
             ),
@@ -22,7 +29,7 @@ class ProfilPage extends StatelessWidget {
               children: <Widget>[
                 Container(
                   alignment: Alignment.topLeft,
-                  child: Text('Profil utilisateur',
+                  child: Text('Profil utilisateur de <PSEUDO>',
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20
@@ -31,11 +38,40 @@ class ProfilPage extends StatelessWidget {
                 ),
               ],
             ),
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Image.asset(
+                    'assets/images/avatar_test_2.png',
+                    height: sizeAvatar,
+                    width: sizeAvatar,
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      child: Text(
+                        'Prénom :',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                          '<PRENOM>'
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ],
         ),
       ),
     );
   }
-
-
 }
