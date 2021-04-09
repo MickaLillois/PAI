4aa56f9413a5d808468d6462f43d3947479d1f01
mathieurudy12 <66941346+mathieurudy12@users.noreply.github.com>
2021-04-09 11:57:42 +0200

    ADD : Logo + UPDATE : Page de profil

--- clientFlutter/flutter_quizinmobile/lib/secondPage.dart
+++ clientFlutter/flutter_quizinmobile/lib/secondPage.dart
@@ -5,12 +5,40 @@ class MySecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : Center(
-        child: Text('Salut à tous'),
-      )
-    );
+      body :
+      Container(
+        child : Column(
+          children: <Widget>[
+            Column(
+              children: [
+                Container(
+                  child: Image.asset('assets/images/logo_with_text.PNG'
 
+                  )
+                ),
+                Container(
+                  child: Text('oui')
+                )
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Text('Profil utilisateur',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                      )
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
- 
+
 }
