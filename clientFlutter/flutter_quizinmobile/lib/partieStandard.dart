237a859d1024456c55fd15a32b375077550467c7
defeverc <43007056+defeverc@users.noreply.github.com>
2021-05-11 17:04:32 +0200

    UPDATE : partieStandard ( récupération des questions et de leurs infos + affichage questions par questions)

--- clientFlutter/flutter_quizinmobile/lib/partieStandard.dart
+++ clientFlutter/flutter_quizinmobile/lib/partieStandard.dart
@@ -2,7 +2,7 @@ import 'dart:collection';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_quizinmobile/util/question.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 
 class PartieStandard extends StatefulWidget{
@@ -16,8 +16,8 @@ class PartieStandard extends StatefulWidget{
 }
 
 class PartieStandardState extends State<PartieStandard>{
-
-  dynamic questions;
+  int cpt=1;
+  HashMap<String, HashMap<String,String>> questions;
 
   Future<String> _makePostRequest(String nbQuestions) async {
     Uri url = Uri.https('quizinmobile.alwaysdata.net', 'Questions/getQuestions.php');
@@ -39,24 +39,120 @@ class PartieStandardState extends State<PartieStandard>{
   }
 
   Future<void> initQuestions() async {
-    questions = jsonDecode(await _makePostRequest('10'));
-    /*questions.forEach((key, value) {
-      print(value['INTITULE']);
-    });*/
+    var questionsTmp = jsonDecode(await _makePostRequest('9'));
+    questionsTmp.forEach((key, value) {
+      print (key);
+    });
+    HashMap<String, HashMap<String,String>> attributs = new HashMap.from(questionsTmp.map(
+            (key, value){
+          HashMap<String,String> attrs = new HashMap.from(value.map(
+                  (key2,value2){
+                return MapEntry(key2.toString(), value2.toString());
+              }
+          ));
+          return MapEntry(
+              key.toString(),
+              attrs
+          );
+        }
+    ));
+    questions = attributs;
+
+    questions.forEach((key, value) {
+      print (key);
+    });
+
+    print(questions);
+    print('init good');
+  }
+
+  String getInfo(String key){
+    String txt = questions["Question$cpt"][key];
+    print(txt);
+    //cpt++;
+    return txt;
+  }
+
+  @override
+  void initState() {
+    initQuestions();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+
+    //Variables de responsivité
+    Size screenSize = MediaQuery.of(context).size;
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    double widthLogo = screenSize.width*0.8;
+    double heightLogo = screenSize.height*0.12;
+    double marginLogo = screenSize.height*0.05;
+    double paddingInput = screenSize.height*0.02;
+
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-          child: Container(
-            child: ElevatedButton(
-              child: Text('Demander questions', style: TextStyle(fontSize: 25.0),),
-              onPressed: () {
-                initQuestions();
-              },
-            ),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(0,marginLogo,0,0),
+                child: Image.asset('assets/images/logo_officiel.png',width: widthLogo, height: heightLogo),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 3,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                        child: Text('Question n°$cpt')
+                    ),
+                    Container(
+                        child: Text(getInfo('INTITULE'))
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Entrez votre réponse',
+                  ),
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  child: Text('Initialiser questions', style: TextStyle(fontSize: 25.0),),
+                  onPressed: () {
+                    initQuestions();
+                  },
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  child: Text('Next question', style: TextStyle(fontSize: 25.0),),
+                  onPressed: () {
+                    if(cpt==9){
+                      Navigator.pop(context);
+                    }else{
+                      setState(() {
+                        cpt ++;
+                      });
+                    }
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
