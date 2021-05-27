08eac728da8e6e7c649c5fd764c464476114b869
defeverc <43007056+defeverc@users.noreply.github.com>
2021-05-27 16:18:37 +0200

    bugfix : lancement de partie fonctionnel sans besoin de recharger la page

--- clientFlutter/flutter_quizinmobile/lib/partieStandard.dart
+++ clientFlutter/flutter_quizinmobile/lib/partieStandard.dart
@@ -21,6 +21,7 @@ class PartieStandardState extends State<PartieStandard>{
   int cpt=1;
   HashMap<String, HashMap<String,String>> questions;
   final myControllerRep = TextEditingController();
+  bool initG=false;
   //int nbVie;
 
   Future<String> _makePostRequest(String nbQuestions) async {
@@ -43,44 +44,50 @@ class PartieStandardState extends State<PartieStandard>{
   }
 
   Future<void> initQuestions() async {
-    var questionsTmp = jsonDecode(await _makePostRequest('9'));
-    questionsTmp.forEach((key, value) {
-      print (key);
-    });
-    HashMap<String, HashMap<String,String>> attributs = new HashMap.from(questionsTmp.map(
-            (key, value){
-          HashMap<String,String> attrs = new HashMap.from(value.map(
-                  (key2,value2){
-                return MapEntry(key2.toString(), value2.toString());
+    if(!initG) {
+      var questionsTmp = jsonDecode(await _makePostRequest('9'));
+      questionsTmp.forEach((key, value) {
+        print(key);
+      });
+      HashMap<String, HashMap<String, String>> attributs = new HashMap.from(
+          questionsTmp.map(
+                  (key, value) {
+                HashMap<String, String> attrs = new HashMap.from(value.map(
+                        (key2, value2) {
+                      return MapEntry(key2.toString(), value2.toString());
+                    }
+                ));
+                return MapEntry(
+                    key.toString(),
+                    attrs
+                );
               }
           ));
-          return MapEntry(
-              key.toString(),
-              attrs
-          );
-        }
-    ));
-    questions = attributs;
+      questions = attributs;
 
-    questions.forEach((key, value) {
+      /*questions.forEach((key, value) {
       print (key);
-    });
+    });*/
 
-    print(questions);
-    print('init good');
+      print(questions);
+      print('init good');
+      initG=true;
+    }
   }
+
+  /*@override
+  void initState() {
+    initQuestions();
+    //nbVie=int.parse(getInfo('NBREPONSESMAX'));
+    Future.delayed(const Duration(seconds: 2), (){});
+    super.initState();
+  }*/
 
   String getInfo(String key){
     String txt = questions["Question$cpt"][key];
     print(txt);
     //cpt++;
     return txt;
-  }
-
-  @override
-  void initState() {
-    initQuestions();
-    super.initState();
   }
 
   @override
@@ -104,133 +111,143 @@ class PartieStandardState extends State<PartieStandard>{
     double fontSizeInput = screenSize.height*0.035;
     double fontSizeT1 = screenSize.height*0.045;
     double marginNumQ =screenSize.height*0.02;
-    
+
     //autres variables
-    int nbVie=int.parse(getInfo('NBREPONSESMAX'));
+    int nbVie=3;
 
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(0,marginLogo,0,marginLogo),
-                child: Image.asset('assets/images/logo_officiel.png',width: widthLogo, height: heightLogo),
-              ),
-              Container(
-                width: screenSize.width*0.98,
-                height: screenSize.height*0.55,
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 3,
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                        margin: EdgeInsets.fromLTRB(0,marginNumQ,0,marginNumQ),
-                        child: Text('Question n°$cpt',style: TextStyle(fontSize: fontSizeT1),)
-                    ),
-                    Container(
-                        margin: EdgeInsets.fromLTRB(0,marginNumQ,0,marginNumQ),
-                        child: Text(getInfo('INTITULE'),style: TextStyle(fontSize: fontSizeT1),)
-                    ),
-                    Container(
-                      alignment: Alignment.bottomLeft,
-                      child: Row(
-                        children: [
-                          Text(nbVie.toString(),style: TextStyle(fontSize: fontSizeT1/1.1),),
-                          Icon(CupertinoIcons.heart_fill,
-                              size: iconSize/2,),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                width: widthInput2,
-                child: Row(
-                  children: [
-                    Container(
-                      width: widthInput,
-                      child: TextFormField(
-                        style: TextStyle(
-                          fontSize: fontSizeInput,
-                        ),
-                        decoration: InputDecoration(
-                          hintText: 'Entrez votre réponse',
-                          contentPadding: EdgeInsets.fromLTRB(0,paddingInput,0,paddingInput),
-                        ),
-                        controller: myControllerRep,
-                      ),
-                    ),
-                    Container(
-                      width: widthInputIcon,
-                      child: IconButton(
-                          icon: const Icon(Icons.check_box),
-                          color: Colors.black,
-                          iconSize: iconSize,
-                          onPressed: () {
-                            List<String> reps=getInfo('REPONSES').toUpperCase().split('/');
-                            if(reps.contains(myControllerRep.text.toUpperCase())){
-                              print('cest bieng');
-                              if(cpt==9){
-                                Navigator.pop(context);
-                              }else{
-                                setState(() {
-                                  cpt ++;
-                                });
-                              }
-                              myControllerRep.clear();
-                            }else{
-                              if(nbVie==1){
-                                setState(() {
-                                  cpt ++;
-                                });
-                              }else{
-                                setState(() {
-                                  nbVie --;
-                                });
-                              }
-                              print('cest pas bieng');
-                            }
+      body: FutureBuilder(
+        future: initQuestions(),
+        builder: (context, snapshot) {
+          if (questions==null) {
+            return Center(child:Text("Chargement...",style: TextStyle(fontSize: fontSizeT1/1.1),));
+          }
+          else {return SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
 
-                          }
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0,marginLogo,0,marginLogo),
+                    child: Image.asset('assets/images/logo_officiel.png',width: widthLogo, height: heightLogo),
+                  ),
+                  Container(
+                    width: screenSize.width*0.98,
+                    height: screenSize.height*0.55,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 3,
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                            margin: EdgeInsets.fromLTRB(0,marginNumQ,0,marginNumQ),
+                            child: Text('Question n°$cpt',style: TextStyle(fontSize: fontSizeT1),)
+                        ),
+                        Container(
+                            margin: EdgeInsets.fromLTRB(0,marginNumQ,0,marginNumQ),
+                            child: Text(getInfo('INTITULE'),style: TextStyle(fontSize: fontSizeT1),)
+                        ),
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          child: Row(
+                            children: [
+                              Text(nbVie.toString(),style: TextStyle(fontSize: fontSizeT1/1.1),),
+                              Icon(CupertinoIcons.heart_fill,
+                                size: iconSize/2,),
+                            ],
                           ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: widthInput2,
+                    child: Row(
+                      children: [
+                        Container(
+                          width: widthInput,
+                          child: TextFormField(
+                            style: TextStyle(
+                              fontSize: fontSizeInput,
+                            ),
+                            decoration: InputDecoration(
+                              hintText: 'Entrez votre réponse',
+                              contentPadding: EdgeInsets.fromLTRB(0,paddingInput,0,paddingInput),
+                            ),
+                            controller: myControllerRep,
+                          ),
+                        ),
+                        Container(
+                          width: widthInputIcon,
+                          child: IconButton(
+                              icon: const Icon(Icons.check_box),
+                              color: Colors.black,
+                              iconSize: iconSize,
+                              onPressed: () {
+                                List<String> reps=getInfo('REPONSES').toUpperCase().split('/');
+                                if(reps.contains(myControllerRep.text.toUpperCase())){
+                                  print('cest bieng');
+                                  if(cpt==9){
+                                    Navigator.pop(context);
+                                  }else{
+                                    setState(() {
+                                      cpt ++;
+                                      nbVie=int.parse(getInfo('NBREPONSESMAX'));
+                                    });
+                                  }
+                                  myControllerRep.clear();
+                                }else{
+                                  if(nbVie==1){
+                                    setState(() {
+                                      cpt ++;
+                                    });
+                                  }else{
+                                    setState(() {
+                                      nbVie --;
+                                    });
+                                  }
+                                  print('cest pas bieng');
+                                }
+
+                              }
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  /*Container(
                 child: ElevatedButton(
                   child: Text('Initialiser questions', style: TextStyle(fontSize: 25.0),),
                   onPressed: () {
                     initQuestions();
                   },
                 ),
+              ),*/
+                  Container(
+                    child: ElevatedButton(
+                      child: Text('Next question', style: TextStyle(fontSize: 25.0),),
+                      onPressed: () {
+                        if(cpt==9){
+                          Navigator.pop(context);
+                        }else{
+                          setState(() {
+                            cpt ++;
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                child: ElevatedButton(
-                  child: Text('Next question', style: TextStyle(fontSize: 25.0),),
-                  onPressed: () {
-                    if(cpt==9){
-                      Navigator.pop(context);
-                    }else{
-                      setState(() {
-                        cpt ++;
-                      });
-                    }
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );}
+
+        },),
     );
   }
 
